25ed65d595a0ad6a640f4c6f50f873fa408ad274
Sang Nguyễn <nguyentusang18112001@gmail.com>
2023-07-15 20:32:58 +0700

    add_data

---
+++ data.ts
@@ -0,0 +1,146 @@
+const data = [
+    {
+        "_id": "64afba1ce9ddcd3b8fa1086f",
+        "title": "sang12345",
+        "columns": [
+            {
+                "_id": "64aff3a935b2f8b905ed8ac7",
+                "boardId": "64afba1ce9ddcd3b8fa1086f",
+                "title": "column_12"
+            },
+            {
+                "_id": "64b00c6a8181b245ee3fbaf8",
+                "boardId": "64afba1ce9ddcd3b8fa1086f",
+                "title": "column_3"
+            }
+        ]
+    },
+    {
+        "_id": "64b006eb8181b245ee3fbaf6",
+        "title": "sang1234",
+        "columns": [
+            {
+                "_id": "64b0b50f24a9aecd7bab399b",
+                "title": "dwqwddwqdw",
+                "boardId": "64b006eb8181b245ee3fbaf6"
+            },
+            {
+                "_id": "64b0b56f24a9aecd7bab399c",
+                "title": "dwqqddw",
+                "boardId": "64b006eb8181b245ee3fbaf6"
+            }
+        ]
+    },
+    {
+        "_id": "64b00c648181b245ee3fbaf7",
+        "title": "sang143deedwd",
+        "columns": [
+            {
+                "_id": "64b00cba8181b245ee3fbafc",
+                "boardId": "64b00c648181b245ee3fbaf7",
+                "title": "column_7"
+            },
+            {
+                "_id": "64b28455a8e2cb917e615b3b",
+                "title": "fwefe",
+                "boardId": "64b00c648181b245ee3fbaf7"
+            }
+        ]
+    },
+    {
+        "_id": "64b2864da8e2cb917e615b44",
+        "title": "fdedwq",
+        "columns": [
+            {
+                "_id": "64b287caa8e2cb917e615b4a",
+                "title": "cewfew",
+                "boardId": "64b2864da8e2cb917e615b44"
+            }
+        ]
+    },
+    {
+        "_id": "64b2877ba8e2cb917e615b48",
+        "title": "dwqdqw",
+        "columns": []
+    },
+    {
+        "_id": "64b287c5a8e2cb917e615b49",
+        "title": "fqfdwe",
+        "columns": []
+    }
+]
+
+const dataBoards = [
+    {
+        "_id": "64afba1ce9ddcd3b8fa1086f",
+        "title": "sang12345",
+    },
+    {
+        "_id": "64b006eb8181b245ee3fbaf6",
+        "title": "sang1234",
+    },
+    {
+        "_id": "64b00c648181b245ee3fbaf7",
+        "title": "sang143deedwd",
+    },
+    {
+        "_id": "64b2864da8e2cb917e615b44",
+        "title": "fdedwq",
+    },
+    {
+        "_id": "64b2877ba8e2cb917e615b48",
+        "title": "dwqdqw",
+    },
+    {
+        "_id": "64b287c5a8e2cb917e615b49",
+        "title": "fqfdwe",
+    }
+]
+
+const dataColumns = [
+    {
+        "_id": "64aff3a935b2f8b905ed8ac7",
+        "boardId": "64afba1ce9ddcd3b8fa1086f",
+        "title": "column_12"
+    },
+    {
+        "_id": "64b00c6a8181b245ee3fbaf8",
+        "boardId": "64afba1ce9ddcd3b8fa1086f",
+        "title": "column_3"
+    },
+    {
+        "_id": "64b00cba8181b245ee3fbafc",
+        "boardId": "64b00c648181b245ee3fbaf7",
+        "title": "column_7"
+    },
+    {
+        "_id": "64b0b50f24a9aecd7bab399b",
+        "title": "dwqwddwqdw",
+        "boardId": "64b006eb8181b245ee3fbaf6"
+    },
+    {
+        "_id": "64b0b56f24a9aecd7bab399c",
+        "title": "dwqqddw",
+        "boardId": "64b006eb8181b245ee3fbaf6"
+    },
+    {
+        "_id": "64b0b74324a9aecd7bab399d",
+        "title": "t2r",
+        "boardId": "64b006eb8181b245ee3fbaf6"
+    },
+    {
+        "_id": "64b0b75b24a9aecd7bab399e",
+        "title": "dddew",
+        "boardId": "64b006eb8181b245ee3fbaf6"
+    },
+    {
+        "_id": "64b0ba2624a9aecd7bab399f",
+        "title": "column 5",
+        "boardId": "64afba1ce9ddcd3b8fa1086f"
+    },
+    {
+        "_id": "64b0d58b2527967b186f050b",
+        "boardId": "64afba1ce9ddcd3b8fa1086f",
+        "title": "column_10"
+    }
+]
